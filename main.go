9f7d4438ec938190d1ed4512fc251f28d141319d
Andraz Vrhovec <andraz@koofr.net>
2014-12-12 11:04:02 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+
+	var dir string
+	if len(os.Args) > 1 {
+		dir = os.Args[1]
+	} else {
+		dir, _ = os.Getwd()
+	}
+
+	fi, err := ioutil.ReadDir(dir)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, f := range fi {
+		fmt.Printf("%s\t%+q\t%s\n", f.Name(), f.Name(), hex(f.Name()))
+	}
+
+}
+
+func hex(in string) (out string) {
+
+	for i := 0; i < len(in); i++ {
+		out = out + fmt.Sprintf("% X", in[i])
+	}
+
+	return
+}
